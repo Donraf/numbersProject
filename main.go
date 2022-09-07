40a4881c75a924d1311cbfbcecf3ca309e6d2567
Donraf <maxim.anikin1@yandex.ru>
2022-09-07 13:53:16 +0300

    Added operators sorting

--- main.go
+++ main.go
@@ -8,18 +8,6 @@ type Operator struct {
 	Priority int
 }
 
-//func GetOperators(operators *string, arr *string) []string {
-//	operatorSlice := make([]string, 0)
-//	for _, elemInArr := range *arr {
-//		for _, operator := range *operators {
-//			if operator == elemInArr {
-//				operatorSlice = append(operatorSlice, string(elemInArr))
-//			}
-//		}
-//	}
-//	return operatorSlice
-//}
-
 func GetOperators(operators *string, arr *string) []Operator {
 	operatorsSlice := make([]Operator, 0)
 	var number int = 1
@@ -42,6 +30,10 @@ func GetOperators(operators *string, arr *string) []Operator {
 					basePriority = 1
 				case '/':
 					basePriority = 1
+				case '%':
+					basePriority = 1
+				case '^':
+					basePriority = 1
 				}
 				var newOperator Operator = Operator{
 					Type:     string(elemInArr),
@@ -53,14 +45,21 @@ func GetOperators(operators *string, arr *string) []Operator {
 			}
 		}
 	}
+	SortOperators(&operatorsSlice)
 	return operatorsSlice
 }
 
-//func SortOperators(operators *[]Operator) *[]Operator {
-//	for _, operator := range *operators {
-//
-//	}
-//}
+func SortOperators(operators *[]Operator) {
+	var operatorsNum int = len(*operators)
+	for i := 1; i < operatorsNum; i++ {
+		var operatorPriotiry int = (*operators)[i].Priority
+		for j := i - 1; j >= 0; j-- {
+			if operatorPriotiry > (*operators)[j].Priority {
+				(*operators)[j], (*operators)[j+1] = (*operators)[j+1], (*operators)[j]
+			}
+		}
+	}
+}
 
 func GetOperands(operators *string, arr *string) []string {
 	operatorSlice := make([]string, 0)
@@ -75,8 +74,9 @@ func GetOperands(operators *string, arr *string) []string {
 }
 
 func main() {
-	var operatorsSet string = "-+*/"
-	var expression string = "1+2*9+2*(8+3)"
+	var operatorsSet string = "-+*/%^"
+	//var expression string = "1+2*9+2*(8+3)"
+	var expression string = "1*2(3*(4+5)*(5+6))*7"
 	var operatorsSlice []Operator
 	operatorsSlice = GetOperators(&operatorsSet, &expression)
 	operandsSlice := make([]int, len(operatorsSlice)+1)
